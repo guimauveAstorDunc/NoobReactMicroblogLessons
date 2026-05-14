20afc6c81d2cd16ed92242fd92f2eb1bfc1dc178
guimauveAstorDunc <case.ccc23@gmail.com>
2026-05-14 16:20:47 -0400

    Using Array.map() as For-Each Loop to Render Fake Posts.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 
 export default function App() {
-  const post = {
-    id: 1,
-    text: 'Hello, world!',
-    timestamp: 'a minute ago',
-    author: {
-      username: 'susan',
+  const posts = [
+    {
+      id: 1,
+      text: 'Hello, world!',
+      timestamp: 'a minute ago',
+      author: {
+        username: 'susan',
+      },
     },
-  }
+    {
+      id: 2,
+      text: 'Second post',
+      timestamp: 'an hour ago...don\'t worry about the time',
+      author: {
+        username: 'john',
+      },
+    }
+  ];
 
   return (
-    <> {/* Fragment: Invisible Parent Elmt */}
+    // Fragment: Invisible Parent Elmt
+    <>
       <h1>Microblog</h1>
-      <p>
-        <b>{post.author.username}</b> &mdash; {post.timestamp}
-        <br />
-        {post.text}
-      </p>
+      {posts.map(post => {
+        return (
+          <p key={post.id}>
+            <b>{post.author.username}</b> &mdash; {post.timestamp}
+            <br />
+            {post.text}
+          </p>
+        );
+      })}
     </>
   );
 }
