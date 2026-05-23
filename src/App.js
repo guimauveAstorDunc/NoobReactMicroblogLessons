351533c8cd7bfa7466ec977863648cfaed3889c3
guimauveAstorDunc <case.ccc23@gmail.com>
2026-05-23 17:41:14 -0400

    Restructuring App.js to be more Condensed.

--- src/App.js
+++ src/App.js
@@ -2,51 +2,25 @@ import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Posts from './components/Posts';
 
 export default function App() {
-  const posts = [
-    {
-      id: 1,
-      text: 'Hello, world!',
-      timestamp: 'a minute ago',
-      author: {
-        username: 'susan',
-      },
-    },
-    {
-      id: 2,
-      text: 'Second post',
-      timestamp: 'an hour ago...don\'t worry about the time',
-      author: {
-        username: 'john',
-      },
-    }
-  ];
-
   return (
-    // Fragment: Invisible Parent Elmt
+    // Parent Container
     <Container fluid className="App">
-      <Header />
+      <Header /> {/* Navbar */}
+
       <Container>
         <Stack direction="horizontal">
           <Sidebar />
+
           <Container>
-            {posts.length === 0 ?
-              <p>There are no blog posts.</p>
-            :
-              posts.map(post => {
-                return (
-                  <p key={post.id}>
-                    <b>{post.author.username}</b> &mdash; {post.timestamp}
-                    <br />
-                    {post.text}
-                  </p>
-                );
-              })
-            }
+            <Posts />
           </Container>
+
         </Stack>
       </Container>
+
     </Container>
   );
 }
