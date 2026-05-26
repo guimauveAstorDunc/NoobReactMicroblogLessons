a9c8dbfa2af1f6d961d024935ae359aeb62cf543
guimauveAstorDunc <case.ccc23@gmail.com>
2026-05-26 12:24:09 -0400

    Proper SPA Usr Experience: Links Now Use NavLink.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,22 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { NavLink } from 'react-router-dom';
 
 export default function Sidebar() {
     return (
         <Navbar sticky="top" className="flex-column Sidebar">
             <Nav.Item>
-                <Nav.Link href="/">Feed</Nav.Link>
+                {/* the "end" keyword
+                    indicates that the page is only active
+                    if the URL matches the "to" tag EXACTLY.
+                    
+                    If the homepage didn't have this,
+                    the homepage would always be considered "active,"
+                    because every page extends from the homepage. */}
+                <Nav.Link as={NavLink} to="/" end>Feed</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="/explore">Explore</Nav.Link>
+                <Nav.Link as={NavLink} to="/explore">Explore</Nav.Link>
             </Nav.Item>
         </Navbar>
     );
