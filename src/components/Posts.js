bca92e0bb6bdc9ea25a0f8cf7e9abbdd5ffe2ee5
Casey Carta <case.ccc23@gmail.com>
2026-07-08 16:29:51 -0400

    Posts on User Profile and Explore Page.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,21 +3,35 @@ import Spinner from 'react-bootstrap/Spinner';
 import { useApi } from '../contexts/ApiProvider';
 import Post from './Post'
 
-export default function Posts() {
+export default function Posts({ content }) {
   const [posts, setPosts] = useState();
   const api = useApi();
+
+  let url;
+  switch (content) {
+    case 'feed':
+    case undefined:
+      url = '/feed';
+      break;
+    case 'explore':
+      url = '/posts';
+      break;
+    default:
+      url = `/users/${content}/posts`;
+      break;
+  }
   
   useEffect(() => {
     // Inner Lambda Func called because useState() is NOT async.
     (async () => {
-      const response = await api.get('/feed');
+      const response = await api.get(url);
       if (response.ok) {
         setPosts(response.body.data);
       } else {
         setPosts(null);
       }
     })();
-  }, [api]);
+  }, [api, url]);
 
   return (
     <>
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -5,6 +5,7 @@ import Spinner from 'react-bootstrap/Spinner';
 import { useParams } from 'react-router-dom';
 import Body from '../components/Body';
 import TimeAgo from '../components/TimeAgo';
+import Posts from '../components/Posts';
 import { useApi } from '../contexts/ApiProvider';
 
 export default function UserPage() {
@@ -24,26 +25,29 @@ export default function UserPage() {
             {user === undefined ?
                 <Spinner animation="border"/>
             :
+            <>
+                {user === null ?
+                    <p>Could not retrieve blog posts.</p>
+                :
                 <>
-                    {user === null ?
-                        <p>User not found.</p>
-                    :
-                        <Stack direction="horizontal" gap={4}>
-                            <Image  src={user.avatar_url + '&s=128'}
-                                    width={128} height={128}
-                                    roundedCircle/>
-                            <div>
-                                <h1>{username}</h1>
-                                {user.about_me && <h5>{user.about_me}</h5>}
-                                <p>
-                                    Member since: <TimeAgo isoDate={user.first_seen} />
-                                    <br />
-                                    Last seen: <TimeAgo isoDate={user.last_seen} />
-                                </p>
-                            </div>
-                        </Stack>
-                    }
+                    <Stack direction="horizontal" gap={4}>
+                        <Image  src={user.avatar_url + '&s=128'}
+                                width={128} height={128}
+                                roundedCircle/>
+                        <div>
+                            <h1>{username}</h1>
+                            {user.about_me && <h5>{user.about_me}</h5>}
+                            <p>
+                                Member since: <TimeAgo isoDate={user.first_seen} />
+                                <br />
+                                Last seen: <TimeAgo isoDate={user.last_seen} />
+                            </p>
+                        </div>
+                    </Stack>
+                    <Posts content={user.id} />
                 </>
+                }
+            </>
             }
         </Body>
     );
