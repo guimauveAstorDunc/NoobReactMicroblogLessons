16011d70975665e89d6713e626ded09fd944b750
Casey Carta <case.ccc23@gmail.com>
2026-07-14 08:01:37 -0400

    Inactive Registration Page; Issues with Login Page will be fixed once Users are added.

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Body from '../components/Body';
@@ -6,11 +7,33 @@ import InputField from '../components/InputField';
 
 export default function LoginPage() {
     const [formErrors, setFormErrors] = useState({});
+    const usernameField = useRef();
+    const passwordField = useRef();
+
+    useEffect(() => {
+        usernameField.current.focus();
+    }, []);
 
     const onSubmit = (ev) => {
         ev.preventDefault();
-        console.log('handle form here');
+        const username = usernameField.current.value;
+        const password = passwordField.current.value;
+        
+        const errors = {};
+        if (!username) {
+            errors.username = 'Username must not be empty.';
+        }
+        if (!password) {
+            errors.password = 'Password must not be empty.';
+        }
+        setFormErrors(errors);
+        if (Object.keys(errors).length > 0) {
+            return;
+        }
+
+        // TODO: log the user in
     };
+
     return (
         <Body>
             <h1>Login form</h1>
@@ -23,6 +46,8 @@ export default function LoginPage() {
                     error={formErrors.password} />
                 <Button variant="primary" type="submit">Login</Button>
             </Form>
+            <hr />
+            <p>Don&apos;t have an account? <Link to="/register">Register here</Link>!</p>
         </Body>
     );
 }
