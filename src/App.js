46c5feb09b5faecaed6182d4f760ba4234d309fe
Casey Carta <case.ccc23@gmail.com>
2026-07-29 09:30:39 -0400

    Password Reset Request via Email; End of Chapter 9.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+// APIs
 import ApiProvider from './contexts/ApiProvider';
 import FlashProvider from './contexts/FlashProvider';
 import UserProvider from './contexts/UserProvider';
+// COMPONENTS
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
+// PAGES
 import FeedPage from './pages/FeedPage';
 import ExplorePage from './pages/ExplorePage';
 import UserPage from './pages/UserPage';
@@ -13,6 +16,8 @@ import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
 import EditUserPage from './pages/EditUserPage';
 import ChangePasswordPage from './pages/ChangePasswordPage';
+import ResetRequestPage from './pages/ResetRequestPage';
+import ResetPage from './pages/ResetPage';
 
 export default function App() {
   return (
@@ -25,11 +30,13 @@ export default function App() {
 
           <Routes>
             <Route path="/login" element={
-              <PublicRoute><LoginPage /></PublicRoute>
-            } />
+              <PublicRoute><LoginPage /></PublicRoute>        } />
             <Route path="/register" element={
-              <PublicRoute><RegistrationPage /></PublicRoute>
-            } />
+              <PublicRoute><RegistrationPage /></PublicRoute> } />
+            <Route path="/reset-request" element={
+              <PublicRoute><ResetRequestPage /></PublicRoute> } />
+            <Route path="/reset" element={
+              <PublicRoute><ResetPage /></PublicRoute>        } />
             <Route path="*" element={
               <PrivateRoute><Routes>
                   <Route path="/" element={<FeedPage />} />
